e454a98729473c55c6a9cb4e04c23d8facd2c437
Caique Souza Fonseca <c.souzaf@hotmail.com>
2025-02-24 20:54:53 -0300

    teste salvar livro

--- src/main/java/AdminLivrosController.java
+++ src/main/java/AdminLivrosController.java
@@ -1,10 +1,21 @@
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
 import models.Livro;
 
+@Named
+@RequestScoped
 public class AdminLivrosController {
+    private Livro livro = new Livro();
 
-    private Livro livro;
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
 
     public void salvar() {
-
+        System.out.println("Salvando livro");
     }
 }
